d51eb1f27e952d04801a594117d12b4aebf82389
palina0k <korzik.polina@gmail.com>
2024-02-09 16:42:50 -0800

    Removed bullet points from navbar, tried different margin. Started compare page, just trying to resize two images

--- src/pages/Compare.js
+++ src/pages/Compare.js
@@ -1,10 +1,41 @@
 import React from 'react'
 import './Compare.css'
 
+function BoxOne(){
+    return(
+        <div className = "box">
+            <PlaceOne />
+        </div>
+    )
+}
+function BoxTwo(){
+    return(
+        <div className = "box">
+            <PlaceTwo />
+        </div>
+    )
+}
+
+function PlaceOne() {
+    return(
+        <div>
+            <img src="https://housing.ucdavis.edu/_images/buildings/slide_shows/the-green-DSC9711.jpg" alt = "The Green at West Village" className = "image-container" />
+        </div>
+    )
+}
+
+function PlaceTwo() {
+    return(
+        <div>
+            <img src="https://theaggie.org/wp-content/uploads/2023/10/orchardpark_ca_Jersain_Medina-scaled.jpg" alt = "The Green at West Village" className = "image-container" />
+        </div>
+    )
+}
 export default function Compare() {
     return (
-        <div>
-            <h1>This is the place to compare your saved listings</h1>
+        <div className = "container">
+            <BoxOne />
+            <BoxTwo />
         </div>
     )
 }
