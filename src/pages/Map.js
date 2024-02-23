ab17e5a60d26eb56f29dd516d5a16926533c76d8
Alex <alexqi321@gmail.com>
2024-02-23 15:49:25 -0800

    Map border and Mobile navbar fix

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -11,7 +11,7 @@ import './Map.css'
 const GooglePageComponent = () => {
     return (
         <div className="google-page">
-          <iframe src="https://www.google.com/maps/d/embed?mid=143FaYqKt4n6PNsugPkgSCaWuwaMumlw&ehbc=2E312F&noprof=1" width="100%" height="100%"></iframe>        
+          <iframe src="https://www.google.com/maps/d/embed?mid=143FaYqKt4n6PNsugPkgSCaWuwaMumlw&ehbc=2E312F&noprof=1" width="100%" height="100%" style={{ border: "none" }}></iframe>        
         </div>
     );
   }
