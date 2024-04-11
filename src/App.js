96dc2ec1ba77e87c69774ea3172a1f075f99b707
Alex <alexqi321@gmail.com>
2024-04-10 17:50:53 -0700

    Added basic api call

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import {
   Routes,
   Route,
@@ -12,20 +12,43 @@ import StartYourSearch from './pages/start-your-search.js';
 import Map from './pages/Map.js';
 import Compare from './pages/Compare.js';
 
+import axios from 'axios';
 
 
 
 
 export default function App() {
+  useEffect(() => {
+    // Fetch data when the component mounts
+    fetchData();
+  }, []);
+
+  const fetchData = () => {
+    // Set up Axios request
+    axios.get('https://localwiki.org/api/v4/pages/', {
+      params: {
+        tags: 'apartment' // Replace 'park' with the tag you want to filter by
+      }
+    })
+    .then(response => {
+      // Handle successful response
+      console.log('Data', response.data);
+    })
+    .catch(error => {
+      // Handle error
+      console.error('Error fetching users:', error);
+    });
+  };
+
   return (
     <>
-        <Navbar/>
-        <Routes>
-            <Route path='/' element={<Home />}></Route>
-            <Route path='/start-your-search' element={<StartYourSearch />}></Route>
-            <Route path='/map' element={<Map />}></Route>
-            <Route path='/compare' element={<Compare />}></Route>
-        </Routes>
+      <Navbar/>
+      <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/start-your-search' element={<StartYourSearch />}></Route>
+          <Route path='/map' element={<Map />}></Route>
+          <Route path='/compare' element={<Compare />}></Route>
+      </Routes>
     </>
   )
 }
