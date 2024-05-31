1d053b552eccf591bf05631804cc30c1f76426b3
Alex <alexqi321@gmail.com>
2024-05-30 20:54:34 -0700

    finished home page and navbar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,113 +13,139 @@ https://blog.hubspot.com/hs-fs/hubfs/Google%20Drive%20Integration/homepage%20des
 
 */
 export default function Home() {
-    return (
-    <div style={styles.container}>
-        <div style={styles.sectionMission}>
-            <img src = "home-logo.png" alt = "Aggie-Ville" style={styles.logo}/>
-            <div style={styles.titlecontainer}>
-                <h1 style={styles.title}>AGGIE VILLE</h1>
-                <h2 style={styles.subtitle}></h2>
-                <p style={styles.description}>We are dedicated to helping students in Davis find a place to live!</p>
-                <Link to="/start-your-search">
-                <button style={styles.button}>Start Searching</button>
-                </Link>
-                {/* <h2 style={styles.subtitle}>Statement</h2>
-                <p style={styles.description}>We are dedicated to helping students in Davis find a place to live!</p>
-                <h2 style={styles.subtitle}>Goals</h2>
-                <p style={styles.description}>Create a user-friendly and easy to navigate website for home searching</p>
-                <p style={styles.description}>Our website is unique because of it minimal style and updated information.</p> */}
+    return(
+        <div className = "container">
+          <div className = "left-container">
+            <div className = "words">
+              <h1 className = "aggie">AGGIE</h1>
+              <h1 className = "ville">VILLE</h1>   
             </div>
+            <hr></hr>
+            <div className = 'description-container'>
+              <span className = "description">The first <em>centralized platform</em> for apartment search in the town of Davis. Streamline your housing hunt with AggieVille.</span>           
+            </div>
+            <div className = 'buttons'>
+              <a href='./start-your-search'><button className='button-one' >Start Your Search</button></a>
+              <a href='./compare'><button className='button-two'>Compare Options</button></a>
+            </div>
+          </div>
+          <div className = "right-container">
+            <img src="./background-final.png" className="background"/>
+            <img className="image-one" src="./apartment1.jpeg"></img>
+            <img className="image-two" src="./apartment2.jpeg"></img>
+            <img className="image-three" src="./apartment3.webp"></img>
+          </div>
         </div>
-      {/* <p style={styles.description}>The best way for students at UC Davis to find a place to call home!</p> */}
-      {/* <div style={styles.imageContainer}>
-        <img src="https://images1.apartments.com/i2/XXU-g_j-d3ottUYXPpVrOUzQZOPJSBaiWnjSOvPMPZk/117/image.jpg" alt="Apartment 1" style={styles.image} />
-        <img src="https://images1.apartments.com/i2/IuqxXfNX0_YqKOPWPFqASBaM9DlCD-P9tcZShXDMaW0/117/the-u-davis-ca-kitchen-living-area.jpg?p=1" alt="Apartment 2" style={styles.image} />
-      </div> */}
-    </div>
-        );
-    };
-        const styles = {
-            container: {
-              textAlign: 'center',
-              padding: '50px',
-              fontFamily: 'Montserrat, sans-serif',
-              minHeight: '100vh',
-              display: 'flex',
-              flexDirection: 'column',
-              justifyContent: 'center',
-              backgroundColor: '#573C12',
-            },
-            button: {
-                padding: '10px 10px',
-                fontSize: '18px',
-                backgroundColor: '#4F7942',
-                border: '2px solid #4F7942',
-                color: 'white',
-                borderRadius: '50px',
-                cursor: 'pointer',
-                textDecoration: 'none',
-                transition: 'background-color 0.3s',
-                marginTop: '20px',
-              },
-            title: {
-              fontSize: '100px',
-              marginTop: '30px',
-              marginBottom: '30px',
-              fontFamily: 'thin',
-              fontFamily: 'Montserrat, sans-serif',
-              //fontMono: "Courier",
-              //fontFamily: 'Roboto, sans-serif',
-              //fontFamily: 'Poppins',
-              //textDecoration: 'underline',
-              padding: '10px',
-              color: '#034607',
-            },
-            description: {
-              fontSize: '20px',
-              marginBottom: '10px',
-            },
-            imageContainer: {
-              display: 'flex',
-              justifyContent: 'center',
-              gap: '20px',
-              marginTop: '30px',
-              marginBottom: '30px',
-            },
-            image: {
-              width: '500px',
-              height: '400px',
-              objectFit: 'cover',
-              borderRadius: '10px',
-            },
-            logo: {
-                width: '600px',
-                height: '500px',
-                objectFit: 'cover',
-                borderRadius: '10px',
-                justifyContent:'flex-start',
-              },
-              subtitle:{
-                fontFamily: 'lora',
-                fontFamily: 'bold',
-                marginTop: '30px',
-                marginBottom: '30px',
-                textDecoration: 'underline',
-              },
-              imageContainer2: {
-                display: 'flex',
-                flexDirection: 'row',
-                gap: '20px',
-                marginTop: '30px',
-                marginBottom: '30px',
-              },
-              sectionMission: {
-                backgroundColor: '#F7EFE0',
-                paddingTop: '20px',
-                paddingBottom: '100px',
-                marginTop: '20px',
-                borderRadius: '50px',
-                textAlign: 'center',
-                boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-              },
-          };
+        
+      );}
+
+
+    // <div style={styles.container}>
+    //     <div style={styles.sectionMission}>
+    //         <img src = "home-logo.png" alt = "Aggie-Ville" style={styles.logo}/>
+    //         <div style={styles.titlecontainer}>
+    //             <h1 style={styles.title}>AGGIE VILLE</h1>
+    //             <h2 style={styles.subtitle}></h2>
+    //             <p style={styles.description}>We are dedicated to helping students in Davis find a place to live!</p>
+    //             <Link to="/start-your-search">
+    //             <button style={styles.button}>Start Searching</button>
+    //             </Link>
+    //             {/* <h2 style={styles.subtitle}>Statement</h2>
+    //             <p style={styles.description}>We are dedicated to helping students in Davis find a place to live!</p>
+    //             <h2 style={styles.subtitle}>Goals</h2>
+    //             <p style={styles.description}>Create a user-friendly and easy to navigate website for home searching</p>
+    //             <p style={styles.description}>Our website is unique because of it minimal style and updated information.</p> */}
+    //         </div>
+    //     </div>
+    //   {/* <p style={styles.description}>The best way for students at UC Davis to find a place to call home!</p> */}
+    //   {/* <div style={styles.imageContainer}>
+    //     <img src="https://images1.apartments.com/i2/XXU-g_j-d3ottUYXPpVrOUzQZOPJSBaiWnjSOvPMPZk/117/image.jpg" alt="Apartment 1" style={styles.image} />
+    //     <img src="https://images1.apartments.com/i2/IuqxXfNX0_YqKOPWPFqASBaM9DlCD-P9tcZShXDMaW0/117/the-u-davis-ca-kitchen-living-area.jpg?p=1" alt="Apartment 2" style={styles.image} />
+    //   </div> */}
+    // </div>
+
+          // };
+    //     const styles = {
+    //         container: {
+    //           textAlign: 'center',
+    //           padding: '50px',
+    //           fontFamily: 'Montserrat, sans-serif',
+    //           minHeight: '100vh',
+    //           display: 'flex',
+    //           flexDirection: 'column',
+    //           justifyContent: 'center',
+    //           backgroundColor: '#573C12',
+    //         },
+    //         button: {
+    //             padding: '10px 10px',
+    //             fontSize: '18px',
+    //             backgroundColor: '#4F7942',
+    //             border: '2px solid #4F7942',
+    //             color: 'white',
+    //             borderRadius: '50px',
+    //             cursor: 'pointer',
+    //             textDecoration: 'none',
+    //             transition: 'background-color 0.3s',
+    //             marginTop: '20px',
+    //           },
+    //         title: {
+    //           fontSize: '100px',
+    //           marginTop: '30px',
+    //           marginBottom: '30px',
+    //           fontFamily: 'thin',
+    //           fontFamily: 'Montserrat, sans-serif',
+    //           //fontMono: "Courier",
+    //           //fontFamily: 'Roboto, sans-serif',
+    //           //fontFamily: 'Poppins',
+    //           //textDecoration: 'underline',
+    //           padding: '10px',
+    //           color: '#034607',
+    //         },
+    //         description: {
+    //           fontSize: '20px',
+    //           marginBottom: '10px',
+    //         },
+    //         imageContainer: {
+    //           display: 'flex',
+    //           justifyContent: 'center',
+    //           gap: '20px',
+    //           marginTop: '30px',
+    //           marginBottom: '30px',
+    //         },
+    //         image: {
+    //           width: '500px',
+    //           height: '400px',
+    //           objectFit: 'cover',
+    //           borderRadius: '10px',
+    //         },
+    //         logo: {
+    //             width: '600px',
+    //             height: '500px',
+    //             objectFit: 'cover',
+    //             borderRadius: '10px',
+    //             justifyContent:'flex-start',
+    //           },
+    //           subtitle:{
+    //             fontFamily: 'lora',
+    //             fontFamily: 'bold',
+    //             marginTop: '30px',
+    //             marginBottom: '30px',
+    //             textDecoration: 'underline',
+    //           },
+    //           imageContainer2: {
+    //             display: 'flex',
+    //             flexDirection: 'row',
+    //             gap: '20px',
+    //             marginTop: '30px',
+    //             marginBottom: '30px',
+    //           },
+    //           sectionMission: {
+    //             backgroundColor: '#F7EFE0',
+    //             paddingTop: '20px',
+    //             paddingBottom: '100px',
+    //             marginTop: '20px',
+    //             borderRadius: '50px',
+    //             textAlign: 'center',
+    //             boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+    //           },
+    //       };
--- src/universal/Navbar.js
+++ src/universal/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react"
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useLocation } from 'react-router-dom';
 import "./Navbar.css"
 export default function Navbar() {
     // create a useState hook for wheter menu bar is clicked
@@ -7,6 +8,28 @@ export default function Navbar() {
     const handleClick = () => {
         setClicked(!clicked)
     }
+
+    const location = useLocation();
+
+    useEffect(() => {
+        const navLinks = document.querySelectorAll('.Navbar--links');
+        const currentPath = location.pathname.split('/').pop();
+
+        navLinks.forEach(link => {
+            var listItem = link.parentElement; // Get the parent <li> element
+            var home = document.querySelector('a[href="./"]');
+            if (currentPath == '') {
+                home.parentElement.classList.add('current-page');
+            } else {
+                home.classList.remove('current-page');
+                if (link.getAttribute('href').includes(currentPath) && currentPath != "" && !listItem.classList.contains("getStarted")) {
+                    listItem.classList.add('current-page');
+                } else {
+                    listItem.classList.remove('current-page');
+                } 
+            }
+        });
+    }, [location]);
     return(
         <>
             <nav className = 'Navbar'>
@@ -17,7 +40,7 @@ export default function Navbar() {
                     <li><a className='Navbar--links' href='./map'>Map</a></li>
                     <li><a className='Navbar--links' href='./start-your-search'>Search</a></li>
                     <li><a className='Navbar--links' href='./compare'>Compare</a></li>
-                    <li className ='getStarted'><a className='Navbar--links' href='./start-your-search'>Get Started</a> </li>
+                    {/* <li className ='getStarted'><a className='Navbar--links' href='./start-your-search'>Get Started</a> </li> */}
                 </ul>
                 <div className="right">
                     <div className="mobile" onClick={handleClick}>
